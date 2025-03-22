5f8325a9c77f2487f360e2db072bca1d3bd806bc
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-03-22 09:48:03 +0545

    Added file to learn list

---
+++ Basics/stringformatting.py
@@ -0,0 +1,7 @@
+a = "Hello World, "
+b = "I am here"
+
+print(a+""+b)
+print("Wow %s and %s" %(a,b))
+print("Welcome {0} from {1}" .format(a,b))
+print("Welcome to {first} from {second}" .format(first=a,second=b))
